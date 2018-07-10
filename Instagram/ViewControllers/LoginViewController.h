f5c27b621e92277fac4edd0af98b1c73c22622df
Sophia Zheng <szheng@fb.com>
2018-07-09 17:17:35 -0700

    user can sign up and see errors if username/password fields are blank or if username already taken

--- Instagram/ViewControllers/LoginViewController.h
+++ Instagram/ViewControllers/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  Instagram
 //
 //  Created by Sophia Zheng on 7/9/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
---
+++ Instagram/ViewControllers/LoginViewController.m
@@ -0,0 +1,85 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by Sophia Zheng on 7/9/18.
+//  Copyright © 2018 Sophia Zheng. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)didTapSignUp:(id)sender {
+    [self registerUser];
+}
+
+- (void)registerUser {
+    // initialize user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    if (![newUser.username isEqual:@""] && ![newUser.password isEqual:@""]) {
+        // call sign up function on the object
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                [self callAlertWithTitle:@"Error" alertMessage:[NSString stringWithFormat:@"%@",error.localizedDescription]];
+            } else {
+                NSLog(@"User registered successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    } else {
+        [self callAlertWithTitle:@"Cannot Sign Up" alertMessage:@"Username and password fields cannot be blank"];
+        NSLog(@"Did not register user");
+    }
+}
+
+- (void)callAlertWithTitle:(NSString *)title alertMessage:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // create OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // handle response here.
+    }];
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{}];
+}
+
+- (IBAction)didTapLogin:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
